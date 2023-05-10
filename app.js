d853eda1eca5c553475d27947092229462eb6586
Anish Khari <anishkhari91@gmail.com>
2023-05-10 12:38:49 +0530

    commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    const parseIp = (req) =>
+    req.headers['x-forwarded-for']?.split(',').shift()
+    || req.socket?.remoteAddress
+
+    console.log(parseIp(req))
+    const ip = parseIp(req);
+    res.render("index",{ip});
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
