83e45070e7071f3fd033f6e6d88d74a955452613
Anish Khari <75875288+anishkharii@users.noreply.github.com>
2023-05-10 12:43:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,6 +13,6 @@ app.get('/', (req, res) => {
     res.render("index",{ip});
 });
 
-app.listen(3000, () => {
+app.listen(3000 || PROCESS.ENV.PORT, () => {
   console.log('Server is running on port 3000');
 });
